bf72b7373597311a66e0afe8da12c150614612b4
Dmitriy Sidorov <d_sidorov@wargaming.net>
2021-08-29 00:16:19 +0300

    Add items create method. Finish L9

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,24 @@
 class ItemsController < ApplicationController
+  layout false
+  skip_before_action :verify_authenticity_token
+
   def index
     @items = Item.all
+
+    render json: @items
+  end
+
+  def create
+    item = Item.create(items_params)
+    if item.persisted?
+      render json: item, status: :created
+    else
+      redner json: item.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def items_params
+    params.permit(:name, :price)
   end
 end
