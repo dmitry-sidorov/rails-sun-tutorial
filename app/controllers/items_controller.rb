7291add103be0da1ad9d01ebae17f07f916930e1
Dmitriy Sidorov <d_sidorov@wargaming.net>
2021-09-02 10:23:10 +0300

    Add edit, update. Finish L12

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
     if item.persisted?
       render json: item, status: :created
     else
-      redner json: item.errors, status: :unprocessable_entity
+      render json: item.errors, status: :unprocessable_entity
     end
   end
 
@@ -20,27 +20,44 @@ class ItemsController < ApplicationController
   end
 
   def show
-    begin
-      @item = Item.find_by!(id: params[:id])
-    rescue ActiveRecord::RecordNotFound
-      render body: "Page not found", status: 404
-    end
+    @item = find_item_by_id(params[:id])
   end
 
   def edit
-    
+    @item = find_item_by_id(params[:id])
   end
 
   def update
-    
+    item = find_item_by_id(params[:id])
+
+    if item.update(items_params)
+      redirect_to item_path
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    
+    item = find_item_by_id(params[:id])
+    item.destroy
+
+    if item.destroyed?
+      redirect_to items_path
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
   end
 
   private
   def items_params
     params.permit(:name, :price)
+  end
+
+  def find_item_by_id(id)
+    begin
+      Item.find_by!(id: id)
+    rescue ActiveRecord::RecordNotFound
+      render body: "Page not found", status: 404
+    end
   end
 end
